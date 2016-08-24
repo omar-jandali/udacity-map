ede705c83d9dd892969a8ac41efb93c616f38cd9
omar jandali <omar jandali>
2016-08-24 02:56:02 -0700

    added all of the objects that are going to be used in the project

--- js/model.js
+++ js/model.js
@@ -17,3 +17,98 @@ There will be more comments specific to different things going on in the app
 // The following is going to include all of the different variables that are going to be used in this project (some may be stored in objects)
 
 // The following object is going to contain all of the different objects that are going to be used in the project
+var Locations = {
+  initialLocation:{
+    center:{
+      lat: 34.04129,
+      lng: -118.263667
+    },
+    zoom: 14
+  },
+  Restaurants:[
+    {
+      name:"Maccheroni Republic",
+      address:{
+        street:"332 South Broadway",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90013"
+      }
+      food:"Italian",
+      location:{
+        lat: 34.050076,
+        lng: -118.248646
+      }
+    },
+    {
+      name:"Baco Mercat",
+      address:{
+        street:"408 South Main Street",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90013"
+      }
+      food:"Spanish-Fusion",
+      location:{
+        lat: 34.047847,
+        lng: -118.247222
+      }
+    },
+    {
+      name:"Revolutionario North African Tacos",
+      address:{
+        street:"1436 West Jefferson Boulevard",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90007"
+      }
+      food:"North African Tacos",
+      location:{
+        lat: 34.025333,
+        lng: -118.298615
+      }
+    },
+    {
+      name:"Mex Peru Gipsy",
+      address:{
+        street:"414 E 12th St",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90015"
+      }
+      food:"Mexican",
+      location:{
+        lat: 34.035217,
+        lng: -118.255887
+      }
+    },
+    {
+      name:"Pie Hole",
+      address:{
+        street:"714 Traction Ave",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90013"
+      }
+      food:"Pie & Bakery",
+      location:{
+        lat: 34.045429,
+        lng: -118.236258
+      }
+    },
+    {
+      name:"Stumptown Coffee",
+      address:{
+        street:"06 S Santa Fe Ave",
+        city:"Los Angeles",
+        state:"CA",
+        zip:"90021"
+      }
+      food:"Coffee Bar",
+      location:{
+        lat: 34.033292,
+        lng: -118.229707
+      }
+    },
+  ]
+}
