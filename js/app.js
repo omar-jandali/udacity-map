faad580a23f95f98b2e32059abd87894264f42d4
omar jandali <omar jandali>
2016-08-24 02:55:00 -0700

    setup the initial structure for the map project

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/*
+ This document was create soley by Omar Jandali for the porpose of Udacity Front End Web Developer Project.
+  API's and framworks used in this document include jQuery, knockout.js, google maps APIi, and ticketmaster API
+*/
+
+/*
+This file will contain most of the javascript that will be the foundation of this project. It is going to be set up using the MVVM:
+  ViewModel: will contain most of the functionaliy of the site including calling the object and neccessary information to display
+  View: this is the initial function call as well as the part that will be display the informaiton in the project
+
+Different frameworks, libraries, and apis that will be used:
+  firameoworks and libraries = backbone.js, jquery, and underscore
+  api = Google Maps Javascript API, Google Street View API, Yelp
+
+There will be more comments specific to different things going on in the app
+ */
+
+// THe following function is going to be the initial function that is going to be called in order to get the map to appear on the screen
---
+++ js/model.js
@@ -0,0 +1,19 @@
+/*
+ This document was create soley by Omar Jandali for the porpose of Udacity Front End Web Developer Project.
+  API's and framworks used in this document include jQuery, knockout.js, google maps APIi, and ticketmaster API
+*/
+
+/*
+This file will contain most of the javascript that will be the foundation of this project. It is going to be set up using the MVVM:
+  Model: contain all of the object and varialbes that will be included in the project
+
+Different frameworks, libraries, and apis that will be used:
+  firameoworks and libraries = backbone.js, jquery, and underscore
+  api = Google Maps Javascript API, Google Street View API, Yelp
+
+There will be more comments specific to different things going on in the app
+ */
+
+// The following is going to include all of the different variables that are going to be used in this project (some may be stored in objects)
+
+// The following object is going to contain all of the different objects that are going to be used in the project
